83ae29722e85789985b682df1b4c883bf1decacd
mferdian <eikhapoetra@gmail.com>
2025-05-24 15:54:13 +0700

    Add Vite build output for production

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    base: '/build/', 
 });
+
