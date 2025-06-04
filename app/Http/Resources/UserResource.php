4d075dbcf446f9b9c375de450a874f06f340ddce
mferdian <eikhapoetra@gmail.com>
2025-06-04 08:40:36 +0700

    perbaikan

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
@@ -15,13 +17,13 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'no_telp' => $this->no_telp,
-            'umur' => $this->umur,
-            'alamat' => $this->alamat,
-            'foto' => $this->profile_photo_url,
+        'id' => $this->id,
+        'name' => $this->name,
+        'email' => $this->email,
+        'no_telp' => $this->no_telp,
+        'umur' => $this->umur,
+        'alamat' => $this->alamat,
+        'foto' => $this->profile_photo ? Storage::url($this->profile_photo) : null,
         ];
     }
 }
