f7bc6958e621f0b5da6ba5c66cf11a1ec0bdcf64
mferdian <eikhapoetra@gmail.com>
2025-06-16 14:25:38 +0700

    Article

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -38,7 +38,33 @@ class ArticleController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Detail artikel ditemukan',
-            'data' => $article
+            'data' => [
+                'id' => $article->id,
+                'vet_id' => $article->vet_id,
+                'judul' => $article->judul,
+                'isi' => $article->isi,
+                'gambar' => $article->gambar,
+                'created_at' => $article->created_at,
+                'updated_at' => $article->updated_at,
+                'vet' => [
+                    'id' => $article->vet->id,
+                    'nama' => $article->vet->nama,
+                    'email' => $article->vet->email,
+                    'foto' => $article->vet->foto,
+                    'foto_url' => $article->vet->foto_url, 
+                    'no_telp' => $article->vet->no_telp,
+                    'alamat' => $article->vet->alamat,
+                    'STR' => $article->vet->STR,
+                    'SIP' => $article->vet->SIP,
+                    'alumni' => $article->vet->alumni,
+                    'harga' => $article->vet->harga,
+                    'jenis_kelamin' => $article->vet->jenis_kelamin,
+                    'tgl_lahir' => $article->vet->tgl_lahir,
+                    'deskripsi' => $article->vet->deskripsi,
+                    'created_at' => $article->vet->created_at,
+                    'updated_at' => $article->vet->updated_at,
+                ]
+            ]
         ]);
     }
 }
